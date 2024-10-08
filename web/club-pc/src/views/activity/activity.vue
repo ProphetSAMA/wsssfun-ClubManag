63e842a721cb0594beba21a70ccd3599c3ac39c7
Wsssfun <3085971975@qq.com>
2024-10-08 23:12:31 +0800

    Update activity.vue

--- web/club-pc/src/views/activity/activity.vue
+++ web/club-pc/src/views/activity/activity.vue
@@ -1,13 +1,15 @@
 <template>
-    <div>
-        活动
-    </div>
+  <div>活动</div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-
+<style scoped lang="scss">
+.news-item {
+    display: flex;
+    height: 110px;
+    padding: 30px 0px;
+    border-bottom: 1px solid #e4e7ed;
+    cursor: pointer;
+}
 </style>
