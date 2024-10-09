4d45f66b3f222fd5e60eb3835512b2955c308b9d
Wsssfun <3085971975@qq.com>
2024-10-09 17:38:51 +0800

    搭建管理后台

---
+++ web/club-admin/src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Layout from '@/components/HelloWorld.vue'
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/home',
+        name: 'home',
+        component: Layout,
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
---
+++ web/club-admin/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+    hmr: true,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      find: '@',
+      replacement: resolve(__dirname, 'src'),
+    },
+  },
+})
