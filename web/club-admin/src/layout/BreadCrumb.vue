86ec813d6c28f840f5056023aa0967a3380952ca
Wsssfun <3085971975@qq.com>
2024-10-12 00:01:00 +0800

    更新 BreadCrumb.vue

--- web/club-admin/src/layout/BreadCrumb.vue
+++ web/club-admin/src/layout/BreadCrumb.vue
@@ -1,15 +1,41 @@
 <template>
-  <el-breadcrumb separator="/">
+  <el-breadcrumb class="bred" separator="/">
 
-    <el-breadcrumb-item>
-      detail
+    <el-breadcrumb-item v-for="item in tabs">
+      {{ item.meta.title }}
     </el-breadcrumb-item>
 
   </el-breadcrumb>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import {onMounted, Ref, ref, watch} from 'vue'
+import {RouteLocationMatched, useRoute} from 'vue-router'
 
+// 获取当前路由
+const route = useRoute()
+// 定义面包屑导航的数据类型
+// Ref:数据类型 ref:响应式数据
+const tabs:Ref<RouteLocationMatched[]> = ref([])
+watch(
+    () => route.path,
+    () => getBredcrumb()
+)
+
+// 获取面包屑导航的数据
+const  getBredcrumb = () => {
+  let mached = route.matched.filter((item) => item.meta && item.meta.title)
+  const first = mached[0]
+  if (first.path !== '/home') {
+    // 构造面包屑导航第一个数据
+    mached = [{ path: '/home', meta: { title: '首页' } } as any].concat(mached)
+  }
+  tabs.value = mached
+}
+// 组件挂载完成后执行
+onMounted(()=>{
+  getBredcrumb()
+})
 </script>
 
 <style lang="scss" scoped>
@@ -17,7 +43,7 @@
   color: white !important;
 }
 .bred {
-  margin-left: 20px
+  margin-left: 20px;
 }
 :deep(.el-breadcrumb_item) {
   font-size: 15px !important;
