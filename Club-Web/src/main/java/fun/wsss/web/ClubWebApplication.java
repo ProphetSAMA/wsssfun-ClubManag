354f590e8c1ad339e484824d055fc38421fee53d
Wsssfun <3085971975@qq.com>
2024-10-21 01:11:35 +0800

    [ Up ] 更新项目依赖

---
+++ Club-Web/src/main/java/fun/wsss/web/ClubWebApplication.java
@@ -0,0 +1,17 @@
+package fun.wsss.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Wsssfun
+ * @date 2024年10月21日 01点10分
+ */
+@SpringBootApplication
+public class ClubWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubWebApplication.class, args);
+	}
+
+}
---
+++ Club-Web/src/test/java/fun/wsss/web/ClubWebApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.wsss.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
