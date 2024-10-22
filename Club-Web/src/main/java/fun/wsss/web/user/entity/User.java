03c520bc22fb590df3c7514dca57cbb241cdf58a
Wsssfun <3085971975@qq.com>
2024-10-22 23:58:47 +0800

    [ Up ] 测试dingtalk通知

--- Club-Web/src/main/java/fun/wsss/web/user/entity/User.java
+++ Club-Web/src/main/java/fun/wsss/web/user/entity/User.java
@@ -1,10 +1,15 @@
 package fun.wsss.web.user.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
 /**
  * @author Wsssfun
  * @date 2024/10/22 23:55
  */
 
+@Data
+@TableName("sys_user")
 public class User {
     private Long id;
     private String username;
