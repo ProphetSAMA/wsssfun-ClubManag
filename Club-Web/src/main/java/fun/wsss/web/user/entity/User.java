a7d75dabd8a9b5a3ab8b92b6866fb174c40788b5
Wsssfun <3085971975@qq.com>
2024-10-22 23:56:19 +0800

    [ Create ] 创建用户相关模块

---
+++ Club-Web/src/main/java/fun/wsss/web/user/entity/User.java
@@ -0,0 +1,17 @@
+package fun.wsss.web.user.entity;
+
+/**
+ * @author Wsssfun
+ * @date 2024/10/22 23:55
+ */
+
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+    private String phone;
+    private String avatar;
+    private Integer status;
+}
