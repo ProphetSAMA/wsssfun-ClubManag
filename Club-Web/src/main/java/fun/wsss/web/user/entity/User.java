fb770582bbdbe6ff7a02035f31f58bee176fa150
Wsssfun <3085971975@qq.com>
2024-10-23 00:10:38 +0800

    [ Up ] 规范实体类编码

--- Club-Web/src/main/java/fun/wsss/web/user/entity/User.java
+++ Club-Web/src/main/java/fun/wsss/web/user/entity/User.java
@@ -1,22 +1,67 @@
 package fun.wsss.web.user.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 /**
- * @author Wsssfun
- * @date 2024/10/22 23:55
+ * 用户实体类
+ *
+ *  @author Wsssfun
+ *  @date 2024/10/22 23:55
  */
-
 @Data
 @TableName("sys_user")
 public class User {
+
+    /**
+     * 主键ID
+     */
+    @TableId(value = "id")
     private Long id;
+
+    /**
+     * 用户名
+     */
+    @TableField("username")
     private String username;
+
+    /**
+     * 密码
+     */
+    @TableField("password")
     private String password;
+
+    /**
+     * 昵称
+     */
+    @TableField("nickname")
     private String nickname;
-    private String email;
+
+    /**
+     * 性别
+     */
+    @TableField("sex")
+    private String sex;
+
+    /**
+     * 手机号
+     */
+    @TableField("phone")
     private String phone;
-    private String avatar;
+
+    /**
+     * 状态
+     */
+    @TableField("status")
     private Integer status;
+
+    /**
+     * 是否为管理员
+     */
+    @TableField("is_admin")
+    private Integer isAdmin;
+}
+
 }
