9087e13725c10371f3cdc769bb50c9b392098aca
Wsssfun <3085971975@qq.com>
2024-10-29 02:34:06 +0800

    [ Create ] 新增表格CURD操作

--- web/club-admin/src/views/user/index.vue
+++ web/club-admin/src/views/user/index.vue
@@ -14,6 +14,19 @@
     <!-- 表格-->
     <el-table :data="tableList" border style="width: 100%" stripe>
       <el-table-column prop="nickname" label="姓名"></el-table-column>
+      <el-table-column prop="sex" label="性别">
+        <template #default="scope">
+          <el-tag v-if="scope.row.sex == '0'" type="danger" size="default">男</el-tag>
+          <el-tag v-if="scope.row.sex == '1'" type="danger" size="default">女</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column prop="phone" label="联系方式"></el-table-column>
+      <el-table-column label="操作" width="220" align="center">
+        <template #default="scope">
+          <el-button type="primary" size="small" @click="editBtn(scope.row)">编辑</el-button>
+          <el-button type="danger" size="small" @click="deleteBtn(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
     </el-table>
     <!-- 新增 -->
     <AddUser ref="addUserRef"/>
